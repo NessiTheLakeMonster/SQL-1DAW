4293bb19d990576e70c900bf8459d722e597eed9
NessiTheLakeMonster <72609546+NessiTheLakeMonster@users.noreply.github.com>
2023-03-13 18:22:23 +0100

    cambios

--- Notas de clase/11-13mar.sql
+++ Notas de clase/11-13mar.sql
@@ -1,4 +1,28 @@
 -- * 13 MARZO
 
+-- FUNCIONES CON DATE
 -- Formatear una date a una cadena de caracteres
+SELECT TO_CHAR(SYSDATE, 'DD-MON-YYYY') FROM DUAL;
 -- Formatear una cadena en una date
+
+SELECT SYSDATE FROM DUAL;
+
+-- Con la fecha de hoy sacar el mes en el que se esta
+SELECT TO_CHAR(SYSDATE, 'MM') +2 FROM DUAL;
+
+-- Cuantos dias tengo
+SELECT SYSDATE - TO_DATE('02/03/01') FROM DUAL;
+
+-- Cuantos años tengo
+SELECT (SYSDATE-TO_DATE('02/03/01'))/365 FROM DUAL;
+
+-- La funcion ADD_MONTHS suma o resta meses
+SELECT ADD_MONTHS(SYSDATE, -6) FROM DUAL;
+
+-- La funcion LAST_DAY coge el ultimo dia del mes de la fecha especificada
+SELECT LAST_DAY(TO_DATE('03/01', 'MM/YY')) FROM DUAL;
+
+-- Cuantos meses he vivido
+SELECT
+
+-- Funcion NEXT_DAY
